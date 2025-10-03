1531e5c90eb41355f877250367ef3a952930d991
QYUbit <qulijazi@gmail.com>
2025-10-03 23:55:12 +0200

    Update release.js

--- scripts/release.js
+++ scripts/release.js
@@ -5,17 +5,21 @@ const packages = ["core", "navigation", "store", "router"]
 const newVersion = process.argv[2]
 
 if (!newVersion) {
-    console.error("version argument is missing")
-    process.exit(1)
+  console.error("version argument is missing")
+  process.exit(1)
 }
 
 packages.forEach((_package) => {
-    const pkgJson = `./packages/${_package}/package.json`
+  const pkgDir = `./packages/${_package}`;
+  const pkgJson = `${pkgDir}/package.json`;
 
-    const raw = fs.readFileSync(pkgJson)
+  const raw = fs.readFileSync(pkgJson);
+  const content = JSON.parse(raw.toString());
+  content.version = newVersion.trim();
+  fs.writeFileSync(pkgJson, JSON.stringify(content, null, 2));
 
-    const content = JSON.parse(raw.toString())
-    content.version = newVersion.trim()
-
-    fs.writeFileSync(pkgJson, JSON.stringify(content, null, 2))
-})
+  execSync(`npm publish --access public`, {
+    cwd: pkgDir,
+    stdio: "inherit",
+  });
+});
